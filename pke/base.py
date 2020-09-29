8e441f5ba0e8c525c3f798a4752897e622f1559f
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2020-09-29 23:41:36 +0530

    Update base.py

--- pke/base.py
+++ pke/base.py
@@ -28,7 +28,7 @@ from builtins import str
 
 get_alpha_2 = lambda l: langcodes.find(l).language
 
-lang_stopwords = {get_alpha_2(l): l for l in stopwords._fileids}
+# lang_stopwords = {get_alpha_2(l): l for l in stopwords._fileids}
 
 lang_stem = {get_alpha_2(l): l for l in set(SnowballStemmer.languages) - set(['porter'])}
 lang_stem.update({'en': 'porter'})
@@ -49,8 +49,8 @@ def get_stopwords(lang):
     """
     global PRINT_NO_STWO_WARNING
     try:
-        lang = lang_stopwords[lang]
-        return stopwords.words(lang)
+        stopwds = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+        return stopwds
     except KeyError:
         if PRINT_NO_STWO_WARNING[lang]:
             logging.warning('No stopwords for \'{}\' language.'.format(lang))
